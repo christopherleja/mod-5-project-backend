dbabd067f8568e747278a7f6fd1c3e250f043cdf
Chris Leja <christopher.leja@gmail.com>
2020-11-01 18:11:26 -0800

    debug autologin

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,11 +6,12 @@ class AuthController < ApplicationController
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      
+
       render json: user
     else 
       render json: { error: "Invalid username or password"}, status: :unauthorized
     end
+
   end
 
   def autologin
